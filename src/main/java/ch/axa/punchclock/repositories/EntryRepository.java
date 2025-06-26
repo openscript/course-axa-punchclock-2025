7b616b83da797fdbe096e30526ea919097127acc
Robin <git+r@obin.ch>
2025-06-26 08:45:38 +0000

    chore: add task 3.3

--- src/main/java/ch/axa/punchclock/repositories/EntryRepository.java
+++ src/main/java/ch/axa/punchclock/repositories/EntryRepository.java
@@ -1,16 +1,15 @@
 package ch.axa.punchclock.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import ch.axa.punchclock.models.Entry;
 
-public interface EntryRepository extends CrudRepository<Entry, Long>, EntryRepositoryFilter {
+public interface EntryRepository extends CrudRepository<Entry, Long>, EntryRepositoryCustom, EntryRepositoryFilter {
     Iterable<Entry> findByCategory(@Param("categoryId") long categoryId);   
 
     Iterable<Entry> findByTagsId(Long tagId);
 
-    @Query(value = "SELECT e FROM Entry e WHERE e.description LIKE %:descriptionSearch%")
-    Iterable<Entry> searchByDescription(@Param("descriptionSearch") String descriptionSearch);
+    //@Query(value = "SELECT e FROM Entry e WHERE e.description LIKE %:descriptionSearch%")
+    //Iterable<Entry> searchByDescription(@Param("descriptionSearch") String descriptionSearch);
 }
