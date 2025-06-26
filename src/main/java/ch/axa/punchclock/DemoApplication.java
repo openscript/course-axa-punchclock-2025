813e926c60145b7975b8761f6cf5bfc4b3e784a8
Robin <git+r@obin.ch>
2025-06-26 09:14:58 +0000

    chore: task 3.5

--- src/main/java/ch/axa/punchclock/DemoApplication.java
+++ src/main/java/ch/axa/punchclock/DemoApplication.java
@@ -3,7 +3,10 @@ package ch.axa.punchclock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class DemoApplication {
 
 	public static void main(String[] args) {
